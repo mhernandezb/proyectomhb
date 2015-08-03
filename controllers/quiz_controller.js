ef82ae09555a3183deb844f30c8883e2a39f897b
Miguel Hernandez <miguelhernandez03@gmail.com>
2015-08-03 08:30:45 -0500

    Version 2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,7 @@
 exports.question = function(req, res){
     res.render('quizes/question', {pregunta: 'Capital de Italia'});
+    res.render('quizes/question', {pregunta: 'Inventor del Bombillo'});
+    res.render('quizes/question', {pregunta: 'Red de Redes'});
 };
 
 exports.answer = function(req, res){
@@ -7,9 +9,16 @@ exports.answer = function(req, res){
         res.render('quizes/answer', {respuesta: 'Correcto'});
     else
         res.render('quizes/answer', {respuesta: 'Incorrecto'});
-
+    if(req.query.respuesta === 'Thomas Alba Edison')
+            res.render('quizes/answer', {respuesta: 'Correcto'});
+    else
+          res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    if(req.query.respuesta === 'Internet')
+          res.render('quizes/answer', {respuesta: 'Correcto'});
+    else
+          res.render('quizes/answer', {respuesta: 'Incorrecto'});
 };
 
 exports.author = function(req, res){
-    res.render('author'); 
+    res.render('author');
 }
